51d38d82fd60965707093a468237e97c1b6906a1
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-05-22 23:25:32 +0200

    Sticky nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 var navbar, navbarOffset;
 if (document.body.clientWidth < 640) {
-  navbar = document.querySelector('.top-bar');
-} else {
   navbar = document.querySelector('.title-bar');
+} else {
+  navbar = document.querySelector('.top-bar');
 }
-navbarOffset = navbar.getBoundingClientRect().top;
+navbarOffset = navbar.getBoundingClientRect().top - navbar.clientHeight;
 
 // make navbar sticky after scroll
 document.addEventListener('scroll', function() {
-  if (navbar.scrollTop > navbarOffset) {
-    console.log('stick');
+  if (window.pageYOffset > navbarOffset) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
   }
 });
